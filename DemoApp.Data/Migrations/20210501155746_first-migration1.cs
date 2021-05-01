d8c31c30a456fed5efe28a9358861613b7ccd905
Ugo Umeoke <laryhug209@yahoo.com>
2021-05-01 17:01:52 +0100

    Add project files.

---
+++ DemoApp.Data/Migrations/20210501155746_first-migration1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DemoApp.Data.Migrations
+{
+    public partial class firstmigration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClanId",
+                table: "Samurais",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Clans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClanName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clans", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Samurais_ClanId",
+                table: "Samurais",
+                column: "ClanId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Samurais_Clans_ClanId",
+                table: "Samurais",
+                column: "ClanId",
+                principalTable: "Clans",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Samurais_Clans_ClanId",
+                table: "Samurais");
+
+            migrationBuilder.DropTable(
+                name: "Clans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Samurais_ClanId",
+                table: "Samurais");
+
+            migrationBuilder.DropColumn(
+                name: "ClanId",
+                table: "Samurais");
+        }
+    }
+}
---
+++ DemoApp.Data/SamuraiContex.cs
@@ -0,0 +1,19 @@
+﻿using DemoApp.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DemoApp.Data
+{
+    public class SamuraiContext: DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        private readonly string _connectionString = "Data Source=LAPTOP-R0IBRK5V\\MSSQLSERVER01;Initial Catalog=SamuraiDemo;Integrated Security=True;";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+            // base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
